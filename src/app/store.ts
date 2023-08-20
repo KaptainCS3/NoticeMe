cc1fe98d065654ba743a7ae3b764a9233a8bbd4a
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-20 22:19:37 +0100

    create event slice reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
-const store = configureStore({
-  reducer: {},
+import eventSliceReducer from "../features/eventSlice/event.slice"
+export const store = configureStore({
+  reducer: {
+    eventSlice: eventSliceReducer,
+  },
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+// export default store;
