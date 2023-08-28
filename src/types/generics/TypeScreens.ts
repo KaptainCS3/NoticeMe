51baecb0d618efea3378499b471e7e6c3fad7ed5
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-28 15:28:46 +0100

    initial commit

---
+++ src/types/generics/TypeScreens.ts
@@ -0,0 +1,9 @@
+export type TypeScreens = {
+    Login: undefined;
+    Events: undefined;
+    Home: undefined;
+    Participant: undefined;
+    User: undefined;
+    CreateEvent: undefined;
+    CreateParticipant: undefined
+}
