00d32da452247d285b24eaf7b43cd2fd372068cf
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-28 15:31:23 +0100

    read/write member switch to appropriate branch

--- src/types/participantType/participantType.ts
+++ src/types/participantType/participantType.ts
@@ -1,6 +1,7 @@
 export default interface participantType{
+    user_id: string | number[];
     name: string;
     email: string;
     phone: string;
-    avarta: string;
+    avarta: string | {};
 }
