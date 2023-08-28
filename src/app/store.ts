f8108144c7f0b09195191e4f50f2b5778f233c8b
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-28 15:30:30 +0100

    read/write member switch to appropriate branch

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import eventSliceReducer from "../features/eventSlice/event.slice"
 import participantSliceReducer from '../features/memberSlice/member.slice'
 import loginSliceReducer from '../features/loginSlice/login.slice'
+
 export const store = configureStore({
   reducer: {
     eventSlice: eventSliceReducer,
     memberSlice: participantSliceReducer,
-    loginSlice: loginSliceReducer
+    loginSlice: loginSliceReducer,
   },
+    middleware: (getDefaultMiddleware) => 
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
